b852b5000d6b59edf1284754a7e506caf6800c8f
Kadzia <klaudiadadok@gmail.com>
2018-07-19 21:37:14 +0200

    Kurs C#: Zmienne i ich typy

---
+++ c#/Deklaracja zmiennych/Deklaracja zmiennych/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Deklaracja_zmiennych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skłądnia deklaracji zmiennych:
+            // Typ Nazwa [=wartość];
+
+            int x, y, z = 23;
+            y = 3;
+            x = 10;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
