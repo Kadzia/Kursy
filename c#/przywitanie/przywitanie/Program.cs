d4adc1ce9f43aea6529190d3c78522d84506aa38
Kadzia <klaudiadadok@gmail.com>
2018-07-19 20:24:43 +0200

    kurs C#: Wprowadzenie

---
+++ c#/przywitanie/przywitanie/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace przywitanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wyświetlenie powitania
+            //Wyświetlenie powitania
+            //Wyświetlenie powitania
+            System.Console.WriteLine("WItaj świecie");
+            System.Console.ReadKey();
+        }
+    }
+}
