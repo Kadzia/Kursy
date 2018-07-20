093bfa2e568e89efbe85b8b1c1dd7f77298f3e46
Kadzia <klaudiadadok@gmail.com>
2018-07-20 22:10:22 +0200

    Kurs C#: Wyrażenia algebraiczne/  Konwersja typów

---
+++ c#/Wyrażenia algebraiczne/Konwersja typów (samochody)/Konwersja typów (samochody)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwersja_typów__samochody_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int liczbaMiesiecy = 12;  //stałe należy inicjalizować w trakcie deklaracji
+            int liczbaSamochodow;
+            double sredniaSprzedaz;
+            Console.WriteLine("Podaj roczną sprzedaż samochodów: ");
+            //liczbaSamochodow = Convert.ToInt32(Console.ReadLine());
+            liczbaSamochodow = int.Parse(Console.ReadLine());
+            sredniaSprzedaz = liczbaSamochodow / (double)liczbaMiesiecy;
+            Console.WriteLine(sredniaSprzedaz);
+            Console.ReadKey();
+        }
+    }
+}
