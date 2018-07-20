04bf765a7f61ddb4b975d77587fb15171b1a37ff
Kadzia <klaudiadadok@gmail.com>
2018-07-20 22:05:33 +0200

    Kurs C#: Wyrażenia algebraiczne/  Algebra

---
+++ c#/Wyrażenia algebraiczne/Algebra/Algebra/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyrażenia_algebraiczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Przykładowe wyrażenie: y=2ab-4 w języku C# zapiszemy:
+            int a = 2, b = 3, y;
+            y = 2 * a * b - 4;
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
