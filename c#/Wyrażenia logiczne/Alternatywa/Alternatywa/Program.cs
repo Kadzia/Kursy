19792d8912b5a5ae1fc8443b11ac8e0c02983987
Kadzia <klaudiadadok@gmail.com>
2018-07-21 16:44:26 +0200

    Kurs C#:Wyrażenia logiczne/ Operatory alternatywy

---
+++ c#/Wyrażenia logiczne/Alternatywa/Alternatywa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alternatywa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operator || jest warunkowy
+            int x = 3;
+            bool wynik;
+            wynik = (x >= 2 || x++ >= 4);
+            Console.WriteLine(wynik);
+            Console.WriteLine(x);
+            Console.WriteLine("");
+
+            //Operator | jest bezwarunkowy
+            int X = 3;
+            bool Wynik;
+            Wynik = (X >= 2 | X++ >= 4);
+            Console.WriteLine(Wynik);
+            Console.WriteLine(X);
+            Console.ReadKey();
+        }
+    }
+}
