29b19e4ada310c60a93a544dea0598f7cc1c4587
Kadzia <klaudiadadok@gmail.com>
2018-07-21 15:35:49 +0200

    Kurs C#: Wyrażenia algebraiczne/ Inkrementacja

---
+++ c#/Wyrażenia algebraiczne/Inkrementacja/Inkrementacja/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inkrementacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inkremantacja:");
+            int x = 3, wynik;
+            wynik = ++x * 3;
+            Console.WriteLine(x);
+            Console.WriteLine(wynik);
+
+            Console.WriteLine("");
+            Console.WriteLine("dekrementacja:");
+            int y = 0, z;
+            z = --y * 4;
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey(); 
+        }
+    }
+}
