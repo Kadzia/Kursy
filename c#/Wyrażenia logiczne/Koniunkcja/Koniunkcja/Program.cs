6f615876fde467350c4026b289b5d5ef51c07277
Kadzia <klaudiadadok@gmail.com>
2018-07-21 16:32:12 +0200

    Kurs C#: Wyrażenia logiczne/  Koniunkcja

---
+++ c#/Wyrażenia logiczne/Koniunkcja/Koniunkcja/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Koniunkcja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //& = koniunkcja bezwarunkowa
+            double wiek = 19, kwota = 25;
+            bool kino;
+            kino = (wiek >= 18 & kwota++ >= 20);
+            Console.WriteLine(kino);
+            Console.WriteLine(kwota);
+            Console.WriteLine("");
+
+            //&& = koniunkcja warunkowa
+            double Wiek = 15, Kwota = 25;
+            bool Kino;
+            Kino = (Wiek >= 18 && Kwota++ >= 20);
+            Console.WriteLine(Kino);
+            Console.WriteLine(Kwota);
+            Console.ReadKey();
+        }
+    }
+}
