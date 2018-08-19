02c7f0264e30f8c294ef32debc856c0c2ce02e91
Kadzia <klaudiadadok@gmail.com>
2018-08-19 15:43:06 +0200

    Kurs C#:Proste operacje na tekstach/ Konkatencja

---
+++ c#/05-Proste operacje na tekstach/5.1-Konkatencja/Konkatencja/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Konkatencja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma=" + 5 + 5 + " Iloczyn=" + 5 * 5);
+
+            //Console.WriteLine("Podaj imię: ");
+            //string tekst = Console.ReadLine();
+            //tekst = "Cześć " + tekst;
+            //Console.WriteLine(tekst);
+            //Console.WriteLine(tekst.Length); // wyświetli liczbę znaków
+            Console.ReadKey();
+        }
+    }
+}
