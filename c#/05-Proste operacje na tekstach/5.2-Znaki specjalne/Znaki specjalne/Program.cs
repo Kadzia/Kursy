d972892494bdb642c46ac862a5fe008312b64ab9
Kadzia <klaudiadadok@gmail.com>
2018-08-19 16:02:57 +0200

    Kurs C#:Proste operacje na tekstach/ Znaki specjalne

---
+++ c#/05-Proste operacje na tekstach/5.2-Znaki specjalne/Znaki specjalne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Znaki_specjalne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linia1\nLinia2");        // przejście do nowej linii
+            Console.WriteLine("Kolumna1\tKolumna2");    // \t - tabulacja
+            Console.WriteLine(@"Kolumna1\tKolumna2");   // @ - dosłowna interpretacja
+            Console.WriteLine("C:\\Windows\\Temp");     // podwójny "\\" wstawia "\"
+            Console.WriteLine("Tytuł filmu: \"Dom\"");  // znak \" wstawia znak"
+            Console.ReadKey();
+
+        }
+    }
+}
+
